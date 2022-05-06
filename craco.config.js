4c1e56d95567335cc309cfb96b3ed310692f32b1
Devon Berry <devberry@pdx.edu>
2022-05-06 09:16:35 -0700

    Working https

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fs = require("fs");
+
+
+module.exports = {
+    devServer: {
+        https: {
+            
+            key: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/privkey.pem'),
+            cert: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
+            //ca: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
+            
+            //key: fs.readFileSync("cert.key"),
+            //cert: fs.readFileSync("cert.crt"),
+            //ca: fs.readFileSync("ca.crt"),
+        },
+        host: "192.168.1.11",
+        port: 8000,
+    }
+};
