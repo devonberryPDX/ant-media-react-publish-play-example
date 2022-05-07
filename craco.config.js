610a9c78ea5b15a0adb8b04ffdce183d14863a2e
Devon Berry <devberry@pdx.edu>
2022-05-06 20:30:30 -0700

    Updated settings, added certs

--- craco.config.js
+++ craco.config.js
@@ -6,13 +6,12 @@ module.exports = {
     devServer: {
         https: {
             
-            key: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/privkey.pem'),
-            cert: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
-            //ca: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
-            
-            //key: fs.readFileSync("cert.key"),
-            //cert: fs.readFileSync("cert.crt"),
-            //ca: fs.readFileSync("ca.crt"),
+            //key: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/privkey.pem'),
+            //cert: fs.readFileSync('C:/Certbot/live/berryhousehold.ddns.net/fullchain.pem'),
+
+            key: fs.readFileSync("privkey.pem"),
+            cert: fs.readFileSync("fullchain.pem"),
+
         },
         host: "berryhousehold.ddns.net",
         port: 8000,
